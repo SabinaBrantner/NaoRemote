8304f82be9206e3518fcc4a2571854b7792c19f7
Sabina Brantner <sabina1@aon.at>
2016-05-01 22:06:39 +0200

    Aktualisierung der Projektnamen und der EffortEstimation

---
+++ Server_Client/NaoClientSocket/src/nao/client/socket/NaoClientSocket.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nao.client.socket;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author Sabina
+ */
+public class NaoClientSocket {
+
+    /**
+     * @param args the command line arguments
+     */
+    //192.168.0.142 9559
+    private static String path = "C:\\Users\\Sabina\\Downloads\\behavior.xar";
+    
+    public static void main(String[] args) throws UnknownHostException{
+        int port = 8081;
+        InetAddress ip = InetAddress.getByName("192.168.45.50");
+        FileClient client = new FileClient(ip, port);
+        client.setPath(path);
+        Thread thread = new Thread(client);
+        thread.run();
+        client.closeAll();
+    }
+}
