f14b733dff40cea15c1f138e3176cec83089a79b
Melanie Mühleder <melanie.muehleder@gmail.com>
2016-06-26 19:15:22 +0200

    Added NaoFileServer Path not the best

---
+++ NAOFileServer/NAOFileServer/NaoFileServer/NaoFileServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace NaoFileServer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ NAOFileServer/NAOFileServer/NaoFileServer/NaoFileServer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NaoFileServer.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
