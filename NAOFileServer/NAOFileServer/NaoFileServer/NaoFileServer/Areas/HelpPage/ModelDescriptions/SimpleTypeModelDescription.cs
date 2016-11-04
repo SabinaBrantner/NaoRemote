703ffcfbf1840d7edf17f9d04d79620faae8a171
Sabina Brantner <sabina1@aon.at>
2016-11-05 00:49:35 +0100

    Structure Changes

--- NAOFileServer/NAOFileServer/NaoFileServer/NaoFileServer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NaoFileServer.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
---
+++ Source/NAOFileServer/NAOFileServer/NaoFileServer/NaoFileServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace NaoFileServer.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
